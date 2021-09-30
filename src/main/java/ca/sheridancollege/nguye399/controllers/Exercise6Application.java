61447a7188766826b4b66b2001979b2c3fde4cde
jess-ngu <jess.ng97@gmail.com>
2021-09-29 21:40:25 -0400

    First commit.

---
+++ src/main/java/ca/sheridancollege/nguye399/controllers/Exercise6Application.java
@@ -0,0 +1,13 @@
+package ca.sheridancollege.nguye399.controllers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exercise6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Exercise6Application.class, args);
+	}
+
+}
